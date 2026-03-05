2c3f149a3a70b714cdf4f09b1bb067b93b36d06e
sergei_vokhminov <rreaper@list.ru>
2026-03-05 20:24:21 +0300

    корректировка маршрутов приложения users

--- users/urls.py
+++ users/urls.py
@@ -12,7 +12,7 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path("login/", UserLoginView.as_view(template_name="users/login.html"), name="login"),
-    path("logout/", LogoutView.as_view(next_page="home_pages:home"), name="logout"),
+    path("logout/", LogoutView.as_view(next_page="home_page:home"), name="logout"),
     path("register/", UserRegisterView.as_view(), name="register"),
     path("email_confirm/<str:token>/", email_verification, name="email_confirm"),
     path("register-success", RegistrationSuccessView.as_view(), name="register_success"),
