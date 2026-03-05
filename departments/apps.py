4f59a61621d6f9b58728ff404f13a6e94da01d0a
sergei_vokhminov <rreaper@list.ru>
2026-03-05 16:20:23 +0300

    корректировка модуля apps приложения employees

--- departments/apps.py
+++ departments/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class DepartmentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "departments"
-    verbose_name = "Профиль сотрудника"
+    verbose_name = "Отделы"
