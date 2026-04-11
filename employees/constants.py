ef395d0493e638e9da692f66057444f24b078c08
sergei_vokhminov <rreaper@list.ru>
2026-04-11 11:39:48 +0300

    в приложении employees добавлен модуль с константами

---
+++ employees/constants.py
@@ -0,0 +1,13 @@
+# Значения для выбора статуса сотрудника
+
+at_work = "work"
+on_vacation = "vacation"
+on_sick_leave = "sick_leave"
+truancy = "truancy"
+
+CONDITION_CHOICES = [
+    (at_work, "На работе"),
+    (on_vacation, "В отпуске"),
+    (on_sick_leave, "На больничном"),
+    (truancy, "Прогул"),
+]
